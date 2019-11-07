43fe245e01a235a7f810e1bae429a98422ff9347
omkarjoshi3103 <54397014+omkarjoshi3103@users.noreply.github.com>
2019-11-07 10:47:57 +0530

    public folder build folder

---
+++ build/precache-manifest.0107f03bdb5528615c32ed7aabd3c1e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaccbe294fc5cf5911012f0cd4ef18ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "977f1fa261d18b3655ac",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "bf53b75cebf40f825bee",
+    "url": "/static/css/main.be5e3002.chunk.css"
+  },
+  {
+    "revision": "977f1fa261d18b3655ac",
+    "url": "/static/js/2.95fcf8ca.chunk.js"
+  },
+  {
+    "revision": "bf53b75cebf40f825bee",
+    "url": "/static/js/main.6e7ef936.chunk.js"
+  },
+  {
+    "revision": "09e4f1a92ae281edb8f8",
+    "url": "/static/js/runtime-main.240f28e7.js"
+  }
+]);
